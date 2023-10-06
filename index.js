5d52efc6ef7c8de327a43dad338efe1f114404b0
Kardo J <117020680+KJankowitz@users.noreply.github.com>
2023-10-06 13:10:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.get("/", (req, res) => {
   //Step 1 - Make the get route work and render the index.ejs file.
   res.render("index.ejs");
